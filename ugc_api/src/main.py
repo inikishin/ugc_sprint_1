9897fa319d13ca0244aa97a66527f04cb4c25c44
darkstorn <inikishin@gmail.com>
2022-08-29 13:49:18 +0300

    add auth middleware

--- ugc_api/src/main.py
+++ ugc_api/src/main.py
@@ -7,6 +7,8 @@ from fastapi.responses import ORJSONResponse
 from api.v1 import events
 from core import config
 from core.logger import LOGGING
+from middleware.auth_middleware import AuthMiddleware
+
 
 app = FastAPI(
     title=config.PROJECT_NAME,
@@ -17,6 +19,7 @@ app = FastAPI(
 
 app.include_router(events.router, prefix='/api/v1/events', tags=['events'])
 
+app.add_middleware(AuthMiddleware, auth_url='http://localhost:5000/auth/who')
 
 if __name__ == '__main__':
     # `uvicorn main:app --host 0.0.0.0 --port 8000`
