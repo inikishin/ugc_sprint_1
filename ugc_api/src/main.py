f95330207ba6a3cb8f85c3805b05a482964845ce
darkstorn <inikishin@gmail.com>
2022-08-30 12:25:32 +0300

    move AUTH_URL to env

--- ugc_api/src/main.py
+++ ugc_api/src/main.py
@@ -1,4 +1,4 @@
-import logging
+import os
 
 import uvicorn
 from fastapi import FastAPI
@@ -6,7 +6,6 @@ from fastapi.responses import ORJSONResponse
 
 from api.v1 import events
 from core import config
-from core.logger import LOGGING
 from middleware.auth_middleware import AuthMiddleware
 
 
@@ -19,7 +18,8 @@ app = FastAPI(
 
 app.include_router(events.router, prefix='/api/v1/events', tags=['events'])
 
-app.add_middleware(AuthMiddleware, auth_url='http://localhost:5000/auth/who')
+app.add_middleware(AuthMiddleware,
+                   auth_url=os.getenv('AUTH_URL', 'http://localhost:5000/auth/who'))
 
 if __name__ == '__main__':
     # `uvicorn main:app --host 0.0.0.0 --port 8000`
