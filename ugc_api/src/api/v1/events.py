d37d763c76840376135ec5370a2b1980fae239d7
darkstorn <inikishin@gmail.com>
2022-08-30 10:14:01 +0300

    add endpoints description

--- ugc_api/src/api/v1/events.py
+++ ugc_api/src/api/v1/events.py
@@ -29,28 +29,31 @@ class MovieLastViewTime(BaseModel):
     paused_sec: int
 
 
-@router.post('/send_rating/')
+@router.post('/send_rating/', summary='Отправить рейтинг фильма от пользователя')
 async def send_rating(movie_rating: MovieRating,
                       event_service: EventService = Depends(get_event_service)):
+    """В теле запроса отправляются данные пользовательского рейтинга для фильма."""
     event_service.send_rating(movie_rating.user_id, movie_rating.movie_id, movie_rating.rating)
     return 'ok'
 
 
-@router.post('/send_history/')
+@router.post('/send_history/', summary='Добавить в историю просмотров пользователя фильм')
 async def send_history(movie_history: MovieHistory,
                       event_service: EventService = Depends(get_event_service)):
+    """В теле запроса отправляются timestamp данные последнего просмотра пользователем фильма."""
     event_service.send_history(movie_history.user_id, movie_history.movie_id, movie_history.viewed)
     return 'ok'
 
 
-@router.post('/send_bookmark/')
+@router.post('/send_bookmark/', summary='Добавить фильма в закладки')
 async def send_bookmark(movie_bookmark: MovieBookmark,
                       event_service: EventService = Depends(get_event_service)):
     event_service.send_bookmark(movie_bookmark.user_id, movie_bookmark.movie_id)
     return 'ok'
 
 
-@router.post('/send_last_view_time/')
+@router.post('/send_last_view_time/',
+             summary='Добавить метку времени в секундах на котором пользователь остановил просмотр фильма.')
 async def send_last_view_time(movie_last_view_time: MovieLastViewTime,
                       event_service: EventService = Depends(get_event_service)):
     event_service.send_last_view_time(movie_last_view_time.user_id,
