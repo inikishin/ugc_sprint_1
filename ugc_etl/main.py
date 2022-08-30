8655a1566063ff15ab3c28974bd052f8c26e0de7
darkstorn <inikishin@gmail.com>
2022-08-30 10:25:13 +0300

    update kafka host env name

--- ugc_etl/main.py
+++ ugc_etl/main.py
@@ -11,7 +11,7 @@ class ETL:
         self.transform = Transform()
         self.extract = Extract(transform=self.transform,
                                load=self.load,
-                               host=os.getenv('KAFKA_SERVER', 'localhost:9092'))
+                               host=os.getenv('KAFKA_HOST', 'localhost:9092'))
 
     def run(self):
         self.extract.run()
